14beb918b3eff710a124f45ff5e95cd9d7c90971
Esther Smeke <esthersmeke@gmail.com>
2024-10-30 07:29:36 -0400

    Added Winston logger with error handler and endpoint improvements

--- src/app.js
+++ src/app.js
@@ -27,6 +27,11 @@ mongoose
   });
 
 app.use(express.json());
+app.use((req, res, next) => {
+  logger.http(`${req.method} ${req.url}`);
+  next();
+});
+
 app.use(cookieParser());
 
 app.use("/api/users", usersRouter);
